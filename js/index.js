d2851b324ccc08a2fd6482eba16b038d76c0b113
Ljason <qjdlsp33@naver.com>
2022-12-21 15:40:11 +0900

    response

---
+++ js/index.js
@@ -0,0 +1,81 @@
+console.log('welcome')
+
+
+// DOM Object
+const h4=document.querySelector('#modal h4');
+const img=document.querySelector('#modal figure>img');
+const day=document.querySelector('#modal dl .year');
+const pro=document.querySelector('#modal dl .program');
+const url=document.querySelector('#modal dl .link>a');
+const content=document.querySelector('#modal dl .text');
+
+const open=document.querySelectorAll('#all>figure')
+// console.log(open)
+const close=document.querySelector('#modal>.close')
+// console.log(close)
+const modal=document.querySelector('#modal')
+
+
+
+// 객체를 생성자 함수로 만들 것
+function Modal(title,pic,year,program,href,text){
+    this.title=title;
+    this.pic=pic;
+    this.year=year;
+    this.program=program;
+    this.href=href;
+    this.text=text;
+}
+
+
+
+
+
+
+// 매서드
+
+// 객체안에 있는 정보를 수정하는 행위
+Modal.prototype.action=function(){
+h4.innerHTML=this.title;
+img.setAttribute('src',this.pic);
+day.innerHTML=this.year;
+pro.innerHTML=this.program;
+url.setAttribute('href',this.href);
+url.innerHTML=this.href;
+content.innerHTML=this.text;
+}
+
+
+
+// 인스턴스
+let myModal=[
+new Modal('title01','./images/pic01.png','2022','프로그램1','http://aaa1.com','내용1'),
+new Modal('title02','./images/pic02.png','2021','프로그램2','http://aaa2.com','내용2'),
+new Modal('title03','./images/pic03.png','2023','프로그램3','http://aaa3.com','내용3'),
+new Modal('title04','./images/pic04.png','2024','프로그램4','http://aaa4.com','내용4'),
+new Modal('title05','./images/pic01.png','2025','프로그램5','http://aaa5.com','내용5'),
+new Modal('title06','./images/pic02.png','2026','프로그램6','http://aaa6.com','내용6')
+]
+
+
+
+
+
+
+// event->작업 ->click(figure,figure>img,#modal>.close)
+
+
+// 여러개를 클릭해야되니까 forEach를 써야함.
+open.forEach(function(item,index){
+item.onclick=function(){
+    modal.style.display='block'
+    myModal[index].action();
+}
+})
+
+
+// 한개만 클릭해도 되니까 forEach 안씀.
+close.onclick=function(){
+    modal.style.display='none';
+}
+
